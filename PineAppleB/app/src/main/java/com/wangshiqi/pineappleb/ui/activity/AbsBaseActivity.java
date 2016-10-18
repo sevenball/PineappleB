00fe89a4ea01efe5f246634a53215a37d0c69026
wanglin <dllo@wanglindeMac-mini.local>
2016-10-18 08:52:21 +0800

    改变电量栏颜色与应用颜色一致

--- PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/AbsBaseActivity.java
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/AbsBaseActivity.java
@@ -7,10 +7,13 @@ package com.wangshiqi.pineappleb.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public abstract class AbsBaseActivity extends AppCompatActivity {
@@ -19,6 +22,15 @@ public abstract class AbsBaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(setLayout());// 订制流程
+        // 改变电量栏颜色和应用颜色一致
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        /**
+         * Color.parseColor 将不是int类型的颜色转换为int类型
+         * 为状态栏添加新的颜色
+         */
+        window.setStatusBarColor(Color.parseColor("#F09800"));
 
         initViews();// 初始化组件
 
