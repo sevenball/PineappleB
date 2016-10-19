3e85e887735eb9b6fd67c2ea4f3dd9043bbee418
wanglin <dllo@wanglindeMac-mini.local>
2016-10-19 20:34:28 +0800

    协调布局测试

---
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/focus/DynamicInfoActivity.java
@@ -0,0 +1,24 @@
+package com.wangshiqi.pineappleb.ui.activity.focus;
+
+import com.wangshiqi.pineappleb.R;
+import com.wangshiqi.pineappleb.ui.activity.AbsBaseActivity;
+
+/**
+ * Created by dllo on 16/10/19.
+ */
+public class DynamicInfoActivity extends AbsBaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_dynamic_info;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
