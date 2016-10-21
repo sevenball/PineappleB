2234188dd258887e0ca018b39b4950c78b5bd4f9
dognkui <dllo@DK.local>
2016-10-21 10:57:52 +0800

    完成最火界面的适配器和实体类及布局但是好没有显示为了改下进度所以上传一下在下拉接着写

--- PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/fragment/hotest/HotestFragment.java
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/fragment/hotest/HotestFragment.java
@@ -3,8 +3,13 @@ package com.wangshiqi.pineappleb.ui.fragment.hotest;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.huxq17.swipecardsview.SwipeCardsView;
 import com.wangshiqi.pineappleb.R;
+import com.wangshiqi.pineappleb.model.bean.hotest.HotCardBean;
+import com.wangshiqi.pineappleb.ui.adapter.hotest.HotCardAdapter;
 import com.wangshiqi.pineappleb.ui.fragment.AbsFragment;
+
+import java.util.List;
 
 /**
  * Created by dllo on 16/10/17.
@@ -12,6 +17,9 @@ import com.wangshiqi.pineappleb.ui.fragment.AbsFragment;
 public class HotestFragment extends AbsFragment {
 
     private TextView hotestTitle;
+    private HotCardAdapter cardAdapter;
+    private List<HotCardBean> cardBeens;
+    private SwipeCardsView cardsView;
 
     public static HotestFragment newInstance() {
 
@@ -30,10 +38,12 @@ public class HotestFragment extends AbsFragment {
     @Override
     protected void initView() {
         hotestTitle = byView(R.id.title_tv);
+        cardsView = byView(R.id.hot_swipe_card_view);
     }
 
     @Override
     protected void initDatas() {
         hotestTitle.setText(getResources().getString(R.string.hotest_tv));
+
     }
 }
