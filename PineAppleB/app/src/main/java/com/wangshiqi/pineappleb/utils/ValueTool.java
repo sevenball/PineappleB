639af3375ce661d3a6a44755983f568124dce8bb
dognkui <dllo@DK.local>
2016-10-22 20:55:00 +0800

    最火界面跳转详情页,详情页可以播放一级界面传来的视频网址

--- PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/utils/ValueTool.java
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/utils/ValueTool.java
@@ -4,6 +4,9 @@ package com.wangshiqi.pineappleb.utils;
  * Created by dllo on 16/10/21.
  */
 public class ValueTool {
+    /**
+     *  最火界面卡片的接口
+     */
     public static final String HOT_CARD = "http://m.live.netease.com/bolo/api/rank/hotVideo.htm?userId=-2798972347206426236";
     public static final String HEAD_VP = "http://m.live.netease.com/bolo/api/index/bannerVideoNew.htm"; // 发现-推荐-轮播图
 
