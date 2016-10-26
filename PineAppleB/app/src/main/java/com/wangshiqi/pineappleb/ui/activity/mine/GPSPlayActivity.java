8d9994c51dd42fd96713a4b038601b8691dac558
dognkui <dllo@DK.local>
2016-10-26 18:57:50 +0800

    添加第三方高德地图

--- PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/mine/GPSPlayActivity.java
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/mine/GPSPlayActivity.java
@@ -1,23 +1,60 @@
 package com.wangshiqi.pineappleb.ui.activity.mine;
 
-import com.wangshiqi.pineappleb.ui.activity.AbsBaseActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.wangshiqi.pineappleb.R;
 
 /**
  * Created by dllo on 16/10/26.
  */
-public class GPSPlayActivity extends AbsBaseActivity {
+public class GPSPlayActivity extends Activity {
+    private MapView mMapView;
+    private AMap aMap;
+
+
     @Override
-    protected int setLayout() {
-        return 0;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gps_layout);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
+        mMapView.onCreate(savedInstanceState);
+        //初始化地图变量
+        aMap = mMapView.getMap();
     }
 
     @Override
-    protected void initViews() {
-
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
     }
 
     @Override
-    protected void initDatas() {
-
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
+        mMapView.onResume();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，实现地图生命周期管理
+//        mMapView.onSaveInstanceState(outState);
+//    }
+
+
 }
