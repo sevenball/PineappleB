1b45924e67f7b6d2f84dbc3a8b9ee0ea1fa3ea12
dognkui <dllo@DK.local>
2016-10-29 15:30:43 +0800

    网络判断雏形吐司

---
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/model/net/NetHttpJudge.java
@@ -0,0 +1,11 @@
+package com.wangshiqi.pineappleb.model.net;
+
+/**
+ * Created by dllo on 16/10/28.
+ */
+public class NetHttpJudge {
+    private static boolean networkAvailable;
+
+
+
+}
--- PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/mine/GPSPlayActivity.java
+++ PineAppleB/app/src/main/java/com/wangshiqi/pineappleb/ui/activity/mine/GPSPlayActivity.java
@@ -62,19 +62,19 @@ public class GPSPlayActivity extends Activity implements LocationSource, View.On
         mLocationClient = new AMapLocationClient(this);
         //初始化定位参数
         mLocationOption = new AMapLocationClientOption();
-//设置定位监听
+        //设置定位监听
         mLocationClient.setLocationListener(this);
-//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
         mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-//设置定位间隔,单位毫秒,默认为2000ms
+        //设置定位间隔,单位毫秒,默认为2000ms
         mLocationOption.setInterval(2000);
-//设置定位参数
+        //设置定位参数
         mLocationClient.setLocationOption(mLocationOption);
-// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-// 在定位结束后，在合适的生命周期调用onDestroy()方法
-// 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-//启动定位
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        // 启动定位
 
         nightBtn.setOnClickListener(this);
         locatingBtn.setOnClickListener(this);
@@ -206,7 +206,7 @@ public class GPSPlayActivity extends Activity implements LocationSource, View.On
                 String s = amapLocation.getAddress() + amapLocation.getCountry() + amapLocation.getProvince() + amapLocation.getCity() + amapLocation.getDistrict() + amapLocation.getStreet() + amapLocation.getStreetNum() + amapLocation.getCityCode() + amapLocation.getAdCode() + amapLocation.getAoiName();
                 Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
                 // 这是缩放级别
-                aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
+                aMap.moveCamera(CameraUpdateFactory.zoomTo(20));
                 // 将地图移动到定位点
                 aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(latitude, longitude)));
                 // 点击定位按钮能够将地图的中心移到定位点
